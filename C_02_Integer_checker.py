00251ad6abd149f094367cc548181bbae2d693b1
MonsterML5 <cameronlile0609@gmail.com>
2025-03-15 09:25:21 +1300

    An integer checker

---
+++ C_02_Integer_checker.py
@@ -0,0 +1,38 @@
+def integer_checker(question, low):
+    """A simple integer checker that has a minimum value and accepts <enter>"""
+
+    # Sets up an error message
+    error = f"Please enter a whole number greater than or equal to {low}."
+
+    while True:
+
+        # Asks the question
+        answer = input(question)
+
+        # Allows <enter> as an answer
+        if answer == "":
+            return answer
+
+        # Checks that it can be an integer
+        try:
+            answer = int(answer)
+
+            # If the number is less than the minimum, print an error
+            if answer < low:
+                print(error)
+
+            # If all is good, return the answer
+            else:
+                return answer
+
+        # Prints an error if the answer isn't an integer
+        except ValueError:
+            print(error)
+
+
+# Main routine goes here
+
+# For testing
+number_refined = integer_checker("Number: ", 1)
+print(number_refined)
+
